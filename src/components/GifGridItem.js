dc1d32c55aa2e54854336174071fb1cde04d5548
Rodrigo Rodriguez <rodrigo.rodriguez@marandu.com.ar>
2020-12-30 08:46:11 -0300

    actualizar nombre e icono de la pag

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ( {id, title, url} ) => {
     return (
         <div className='card animate__animated animate__bounce animate__faster'>
-            <img src={url} alt={title}></img>
+            <div className="img"><img src={url} alt={title}></img></div>
             <p className='title'> { title } </p>
         </div>
     )
